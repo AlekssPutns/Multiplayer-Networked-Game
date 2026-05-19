4b1eb7ab52f3ce5fe854b35042e543bc75deea2c
AlekssPutns <159662011+AlekssPutns@users.noreply.github.com>
2026-05-19 05:51:28 +0100

    Test

--- Assets/_Scripts/AnimationEvents.cs
+++ Assets/_Scripts/AnimationEvents.cs
@@ -2,10 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.Events;
 
-/// <summary>
-/// Helper script that allows animation events to be sent as C# events 
-/// so that we can easily listend to them from the PlayerController script.
-/// </summary>
+
 public class AnimationEvents : MonoBehaviour
 {
     public event Action OnChop, OnAnimationDone, OnInteract;
